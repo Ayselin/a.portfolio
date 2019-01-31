9a71476594cc8c914dff6d3309b2402f37b7f7e1
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-01-31 11:39:57 +0000

    message

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-          git My Portfolio 
+          My Portfolio 
           </p>
           <a
             className="App-link"
