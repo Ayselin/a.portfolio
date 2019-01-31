7231d710facc0ff22956d3fc303801202b61891b
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-01-31 12:21:01 +0000

    photo was added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import photo from './me2.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          My Portfolio 
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img src={photo} alt='my photo'/>
       </div>
     );
   }
