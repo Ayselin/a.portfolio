853c4cd76babb4b92e09112f8f9d4a4b276fa707
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-01-31 16:09:19 +0000

    some changes in CSS

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -7,17 +7,19 @@ export default function AboutMe() {
     return (
         <div className='mainpage'>
             <div className='container1'>
-            <h1 className='title'>Front End Developer</h1>
+                <h1 className='title'>Front End Developer</h1>
                 <img className='photo' src={photo} alt='my photo' />
                 <div><h3 className='about'>Hello, My name is Aysel and I am a web developer from London, UK.
                    I am using HTML CSS and Javascript on a day-to-day basis.
                I like to build things that work and look good. I am constantly improving and learning </h3>
                 </div>
-                
                 <div className='container2'>
                     <img className='photo' src={photo2} alt='my photo' />
                     <div><h3>
                         When life is hard, I like to take a step back and either go otside or reaad a book. </h3>
+                    </div>
+                    <div class="work">
+                        <h1> Featured Work</h1>
                     </div>
                 </div>
             </div>
