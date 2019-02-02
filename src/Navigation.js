65fb4f68b0db907af6fe002c2402e710516ef58e
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-02-02 17:11:53 +0000

    some css changes

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import './Navigation.css';
 
 //import './App.css';
 
 
 class Navigation extends Component {
     render() {
+        
         return (
             <nav>
-                <h2>My Portfolio</h2>
+                <h2 className='logo'>{this.props.logoTitle}</h2>
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Portfolio</a></li>
                 </ul>
       </nav>
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
+import './index.css';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Navigation from './Navigation.js';
 
-ReactDOM.render(<Navigation />, document.getElementById('root'));
+ReactDOM.render(<Navigation logoTitle="Welcome to My Portfolio"/>, document.getElementById('root'));
 
 serviceWorker.unregister();
