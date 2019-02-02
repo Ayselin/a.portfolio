f56d53347a32ed99040bfe0bf144d8fc080a2cf7
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-02-02 17:57:53 +0000

    changes in REDME file

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,9 @@
-import React from "react";
-import {Link} from "react-router";
+import React, {Component} from "react";
 
+class Header extends Component {
+    render(){
+        
+    }
+};
 
-export const Header = (props) => {
-    return (
-        <nav className="navbar navbar-default">
-            <div className="container">
-                <div className="nav navbar-nav">
-                    <ul className="nav navbar-nav">
-                        <li><Link to={"/home"}>Home</Link></li>
-                        <li><Link to={"/about"}>About</Link></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
-}
+export default Header;
--- src/Navigation.js
+++ src/Navigation.js
@@ -6,15 +6,17 @@ import './Navigation.css';
 
 class Navigation extends Component {
     render() {
-        
+        const sections = ['Home', 'About', 'Portfolio', 'Contact'];
+        const navLinks = sections.map( section => {
+            return (
+                <li><a href={'#' + section}>{section}</a></li>
+            )
+        })
         return (
             <nav>
-                <h2 className='logo'>{this.props.logoTitle}</h2>
+                <h2 className="logo">{this.props.logoTitle}</h2>
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Portfolio</a></li>
+                    {navLinks}
                 </ul>
       </nav>
 
