68fb12c0ef52dc878382ea82f98c42263fabf2c9
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-02-02 16:11:22 +0000

    Navigation file added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 import './App.css';
 
 
 class App extends Component {
   render() {
-    
-
-
-
-    
+    return (
+      
+    );
 }
-
+}
 
 
 export default App;
---
+++ src/Navigation.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+//import './App.css';
+
+
+class Navigation extends Component {
+    render() {
+        return (
+            <nav>
+                <h2>My Portfolio</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+      </nav>
+
+        );
+    }
+}
+
+
+export default Navigation;
