379cb7dc3f7871e5c44f0fbb8e492b388b00302b
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-02-02 10:59:40 +0000

    react-mdl installed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import AboutMe from './AboutMe';
+
 import './App.css';
 
 
 class App extends Component {
   render() {
-    return (
-      <Router>
-      <div className="App">
-      <Route exact path='/'component={AboutMe} /> 
-      </div>
-      </Router> 
-    );
-  }
+    
+
+
+
+    
 }
 
 
