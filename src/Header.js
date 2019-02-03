c0de0b6a0a36e3610226443a0bcabd38b94c8ef9
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-02-03 10:37:50 +0000

    image was replaced

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,22 @@
 import React, {Component} from "react";
+import Background from './img/moonlight.jpg';
+
+const myStyles = {
+    backgroundImage: `url( ${Background} )`,
+    width: '100%',
+    height: '100vh',
+    backgroundSize: 'cover'
+}
+
 
 class Header extends Component {
     render(){
-        
+        return (
+            <header style= {myStyles}>
+                
+            </header>
+        );
+
     }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Navigation from './Navigation.js';
+import Header from './Header';
 
-ReactDOM.render(<Navigation logoTitle="Welcome to My Portfolio"/>, document.getElementById('root'));
+class App extends Component {
+    render() {
+        return (
+            <div>
+            <Navigation logoTitle="Aysel Rzayeva"/>
+            <Header/>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
