f315371008b16f976d8eb6defe07ec56300286a1
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-02-03 15:14:36 +0000

    n ew div was created in Header.js

--- src/Header.js
+++ src/Header.js
@@ -5,18 +5,24 @@ import './Header.css'
 const myStyles = {
     backgroundImage: `url( ${Background} )`,
     width: '100%',
-    height: '100vh',
+    height: '80vh',
     backgroundSize: 'cover'
 }
 
 
 class Header extends Component {
-    render(){
+    render() {
         return (
-            <header style= {myStyles}>
+            <header style={myStyles}>
+            <div className='paragraph'>
+                <div className='transbox'>
                 <h1>{this.props.title}</h1>
-                <p></p>
+                <p>Hello, My name is Aysel and I am a web developer from London, UK.
+		         I am using HTML CSS and Javascript on a day-to-day basis.
+		         I like to build things that work and look good. I am constantly improving and learning.</p>
                 <a href="#buttom">{this.props.buttom}</a>
+                </div>
+            </div>
             </header>
         );
 
