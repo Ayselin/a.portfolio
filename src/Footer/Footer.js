3b823b6a0d8297777bcc0f794aba7f60143cdb29
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-02-23 15:33:41 +0000

    icons of contacts were changed

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -9,15 +9,28 @@ class Footer extends Component {
     render() {
         return (
             <div className="footer">
-            <div className='footer-icons'>
-                    <a href='https://www.linkedin.com/in/aysel-rzayeva-722a39172/' rel='noopener noreferrer' target='_blank'>
-                        <img className='nav-icon' src={linkedin} alt='guthub'></img>
-                    </a>
-                    <a href='https://github.com/Ayselin' rel='noopener noreferrer' target='_blank'>
-                        <img className='nav-icon'src={gitHub} alt='github'></img>
-                    </a>
-                    <a href='mailto:ayselrzayeva@hotmail.com' ><img className='nav-icon' src={email} alt='email' aria-hidden='true' /> </a>
-            </div>
+            <div className='footer-contact'>
+                        <a className='footer-github'
+                            href='https://github.com/Ayselin'
+                            target="_blank"
+                            rel="noreferrer noopener">
+                            Github
+                        </a>
+                        <a className='footer-linkedin'
+                            href='https://www.linkedin.com/in/aysel-rzayeva-722a39172/'
+                            target="_blank"
+                            rel="noreferrer noopener"
+                        >
+                            LinkedIn
+                        </a>
+                        <a className='footer-email'
+                            href='mailto:ayselrzayeva@hotmail.com'
+                            target="_blank"
+                            rel="noreferrer noopener"
+                        >
+                            Email
+                        </a>
+                    </div>
             </div>
         )
     }
--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
 import './Navigation.css';
-import gitHub from '../img/github.svg';
-import linkedin from '../img/linkedin.svg';
-import email from '../img/email.svg';
+//import gitHub from '../img/github.svg';
+//import linkedin from '../img/linkedin.svg';
+//import email from '../img/email.svg';
 
 class Navigation extends Component {
     render() {
         return (
             <nav>
                 <div className='nav-links'>
-                    <a href='https://www.linkedin.com/in/aysel-rzayeva-722a39172/' rel='noopener noreferrer' target='_blank'>
-                        <img className='nav-icon' src={linkedin} alt='guthub'></img>
-                    </a>
-                    <a href='https://github.com/Ayselin' rel='noopener noreferrer' target='_blank'>
-                        <img className='nav-icon'src={gitHub} alt='github'></img>
-                    </a>
-                    <a href='mailto:ayselrzayeva@hotmail.com' ><img className='nav-icon' src={email} alt='email' aria-hidden='true' /> </a>
-                    
-                </div>
+                        <a className='nav-github'
+                            href='https://github.com/Ayselin'
+                            target="_blank"
+                            rel="noreferrer noopener">
+                            <i class="fab fa-github" />
+                        </a>
+                        <a className='nav-linkedin'
+                            href='https://www.linkedin.com/in/aysel-rzayeva-722a39172/'
+                            target="_blank"
+                            rel="noreferrer noopener"
+                        >
+                            <i class="fab fa-linkedin" />
+                        </a>
+                        <a className='nav-email'
+                            href='mailto:ayselrzayeva@hotmail.com'
+                            target="_blank"
+                            rel="noreferrer noopener"
+                        >
+                            <i class="fas fa-envelope" />
+                        </a>
+                    </div>
                 <ul>
-                   <li><a className='smoothscroll' href='#home'>Home</a></li>
-                   <li><a className='smoothscroll' href='#about'>About</a></li>
-                   <li><a className='smoothscroll' href='#skills'>Skills</a></li>
-                   <li><a className='smoothscroll' href='#projects'>Projects</a></li>
+                    <li><a className='smoothscroll' href='#home'>Home</a></li>
+                    <li><a className='smoothscroll' href='#about'>About</a></li>
+                    <li><a className='smoothscroll' href='#skills'>Skills</a></li>
+                    <li><a className='smoothscroll' href='#projects'>Projects</a></li>
                 </ul>
             </nav>
 
