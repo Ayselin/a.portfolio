8ed42cc0f065ec355e1850b76b45f5dcb9b77c27
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-02-27 13:30:27 +0000

    Updates

--- precache-manifest.e79483bc1975f625e92ad00dd9de5cb6.js
+++ precache-manifest.e79483bc1975f625e92ad00dd9de5cb6.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/a.portfolio/static/media/moonlight.25a4ba94.jpg"
   },
   {
-    "revision": "d114c8a6dcc3e69fe4e7",
-    "url": "/a.portfolio/static/css/main.1f71fcca.chunk.css"
+    "revision": "02f702009d594c68ec86",
+    "url": "/a.portfolio/static/css/main.1c1c77eb.chunk.css"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
@@ -28,8 +28,8 @@ self.__precacheManifest = [
     "url": "/a.portfolio/static/media/codingwoman.94de629e.png"
   },
   {
-    "revision": "d114c8a6dcc3e69fe4e7",
-    "url": "/a.portfolio/static/js/main.d114c8a6.chunk.js"
+    "revision": "02f702009d594c68ec86",
+    "url": "/a.portfolio/static/js/main.02f70200.chunk.js"
   },
   {
     "revision": "1624727a91705ed76ec1e4c81af17261",
@@ -40,6 +40,10 @@ self.__precacheManifest = [
     "url": "/a.portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/a.portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/a.portfolio/static/media/fontawesome-webfont.fee66e71.woff"
   },
@@ -48,15 +52,11 @@ self.__precacheManifest = [
     "url": "/a.portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/a.portfolio/static/media/fontawesome-webfont.674f50d2.eot"
-  },
-  {
     "revision": "0e1f42bcc10b5830c5dd",
     "url": "/a.portfolio/static/css/1.8bfcba10.chunk.css"
   },
   {
-    "revision": "b6f0fbabc08626c45791c1684542f677",
+    "revision": "7de9cc8a875b5ca7957eac86c7cf9732",
     "url": "/a.portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/a.portfolio/precache-manifest.2ca652f38e00f438673e7027f9b27130.js"
+  "/a.portfolio/precache-manifest.e79483bc1975f625e92ad00dd9de5cb6.js"
 );
 
 workbox.clientsClaim();
