4cb212257f58998d1c7d21ccadf99e9231b736b3
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-03-16 09:08:38 +0000

    content  of the paragrath about me was changed.

--- src/About/About.js
+++ src/About/About.js
@@ -5,19 +5,19 @@ import './About.css'
 class About extends Component {
     render() {
         return (
-            <div className='about' id = 'about'>
+            <div className='about' id='about'>
                 <h1 className='about-title'> About Me </h1>
                 <div className='about-content'>
                     <div className='about-img2'>
                     </div>
                     <div className='about-discription'>
-                        <p className='about-paragrath'>I am keen to explore and utilise all of the tools available to make my work effective, 
-                           efficient and industry leading. I like to build things that work and look good. 
-                           I am constantly improving and learning and I believe that anyone can learn from anyone.
-                           Having divided my time between employment and education for the past few months,
-                           I am now looking to work in an environment where I can contribute to a business.I like to build things that work and look good.
-                           I pride myself on finding innovative and modern solutions to problems and I love teamwork.
-                           My approach to work is accuracy and working hard to achieve the goals.</p>
+                        <p className='about-paragrath'>I am a Front End Developer who is passionate about React.js and JavaScript.<br></br>
+                        I am keen to explore and utilise all the tools available to make my work effective, efficient and industry leading.
+                        I am passionate about learning and building something that impacts the way the world works and lives.<br></br>
+                        Having divided my time between employment and education for the past few years, I am now looking to work in an environment where
+                        I can progress and continue my professional development of code.
+                        I do believe that i will add value to any business as I have an excellent work ethic and excel in past paced, high pressured working environment
+                        </p>
                     </div>
                 </div>
             </div>
