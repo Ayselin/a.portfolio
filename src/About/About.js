75218572d282c1bb579d934db02ba17c42c53712
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-03-17 16:02:41 +0000

    correction was made in the paragraph

--- src/About/About.js
+++ src/About/About.js
@@ -16,7 +16,7 @@ class About extends Component {
                         I am passionate about learning and building something that impacts the way the world works and lives.<br></br>
                         Having divided my time between employment and education for the past few years, I am now looking to work in an environment where
                         I can progress and continue my professional development of code.
-                        I do believe that i will add value to any business as I have an excellent work ethic and excel in past paced, high pressured working environment
+                        I do believe that i will add value to any business as I have an excellent work ethic and excel in past paced, high pressured working environment.
                         </p>
                     </div>
                 </div>
