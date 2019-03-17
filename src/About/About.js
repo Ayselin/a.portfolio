b9797c136090c52b46d8ab4649da03e8dad2da8d
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-03-17 16:01:25 +0000

    h1 was changed to p in about me section

--- src/About/About.js
+++ src/About/About.js
@@ -11,13 +11,13 @@ class About extends Component {
                     <div className='about-img2'>
                     </div>
                     <div className='about-discription'>
-                        <h1 className='about-paragrath'>I am a Front End Developer who is passionate about React.js and JavaScript.<br></br>
+                        <p className='about-paragrath'>I am a Front End Developer who is passionate about React.js and JavaScript.<br></br>
                         I am keen to explore and utilise all the tools available to make my work effective, efficient and industry leading.
                         I am passionate about learning and building something that impacts the way the world works and lives.<br></br>
                         Having divided my time between employment and education for the past few years, I am now looking to work in an environment where
                         I can progress and continue my professional development of code.
                         I do believe that i will add value to any business as I have an excellent work ethic and excel in past paced, high pressured working environment
-                        </h1>
+                        </p>
                     </div>
                 </div>
             </div>
